552957ed4232f0779b5661098a9c9dd06b977ed0
McTavo <mar_padilla@ciencias.unam.mx>
2024-03-03 20:52:13 -0600

    All ready

---
+++ README.md
@@ -0,0 +1,3 @@
+# PDM: Tarea 02
+Repositorio dedicado a la entrega de la segunda tarea de la asignatura Programación de Dispositivos Móviles.
+
---
+++ app/src/main/java/com/example/tarea2/textImg.java
@@ -0,0 +1,16 @@
+package com.example.tarea2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Clase principal para mostrar la imagen de caracteres
+ * @author Marco O. Padilla
+ */
+public class textImg extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
